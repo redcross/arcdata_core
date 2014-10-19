23ca0fd62891453ce8410806fcff261cd35be158
John Laxson <jlaxson@mac.com>
2014-10-19 16:40:06 -0700

    require everything

--- lib/core.rb
+++ lib/core.rb
@@ -4,3 +4,10 @@ end
 
 require 'core/core_hash_ext'
 require 'core/engine'
+require 'core/array_accessor'
+require 'core/form_proxy'
+require 'core/nested_hash'
+require 'core/serialized_columns'
+require 'core/version'
+
+require 'core/admin/log_status_index_view'
